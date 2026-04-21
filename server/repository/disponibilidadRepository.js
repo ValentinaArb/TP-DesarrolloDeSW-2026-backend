40c6199d6ac79bd3f2b213a72402c1303e820164
camiramosss <camiramos1810@gmail.com>
2026-04-21 00:40:40 -0300

    Repository y Service bases

---
+++ server/repository/disponibilidadRepository.js
@@ -0,0 +1,62 @@
+import {DisponibilidadHoraria} from "../model/disponibilidad.js";
+
+class DisponibilidadRepository {
+    constructor() {
+        // array vacio por ahora
+        this.disponibilidades = []; 
+    }
+    
+    // CREATE (POST)
+    create(disponibilidad) {
+        this.disponibilidades.push(disponibilidad);
+        console.log("Disponibilidad creada");
+    }
+
+    // DELETE (DELETE)
+    delete(disponibilidadId) {
+        const indiceAEliminar = this._encontrarIndiceDeId(disponibilidadId);
+
+        if(indiceAEliminar !== -1) {
+            this.disponibilidades.splice(indiceAEliminar, 1);
+            console.log("Disponibilidad eliminada");
+        } else {
+            this._errorNoEncontrado();
+        }
+    }
+
+    // READ (GET)
+    // all
+    findAll() {
+        return this.disponibilidades;
+    }
+
+    // by ID
+    findById(disponibilidadId) {
+        const indiceBuscado = this._encontrarIndiceDeId(disponibilidadId);
+
+        if(indiceBuscado !== -1){
+            return this.disponibilidades[indiceBuscado];
+        } else {
+            this._errorNoEncontrado();
+        }
+    }
+
+    // UPDATE (PUT/PATCH)
+    update(nuevaDisponibilidad, idDispoVieja) {
+        this.delete(idDispoVieja);
+        this.create(nuevaDisponibilidad);
+        console.log("Disponibilidad actualizada correctamente.");
+    }
+
+    // metodos internos
+    _encontrarIndiceDeId(disponibilidadId) {    
+        const indice = this.disponibilidades.findIndex((d) => String(d.id) === String(disponibilidadId));
+        return indice;
+    }
+
+    _errorNoEncontrado() {
+        throw new Error("Whoops! El id de disponibilidad buscado no existe");
+    }
+}
+
+export { DisponibilidadRepository };
