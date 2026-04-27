61d164bb7f93b1b22c4ef266fb64fa31980bd8bf
unknown <valen.arbarello@gmail.com>
2026-04-27 09:55:25 -0300

    Errores terminados v.2

--- server/routes/disponibilidadRoutes.js
+++ server/routes/disponibilidadRoutes.js
@@ -3,10 +3,10 @@ import disponibilidadController from "../controllers/disponibilidadController.js
 
 const router = Router();
 
-router.get('/', async (req, res) => await disponibilidadController.obtenerTodas(req, res));
-router.get('/:id', async (req, res) => await disponibilidadController.obtenerDisponibilidad(req, res));
-router.post('/', async (req, res) => await disponibilidadController.crearDisponibilidad(req, res));
-router.delete('/:id', async (req, res) => await disponibilidadController.eliminarDisponibilidad(req, res));
+router.get('/', async (req, res, next) => await disponibilidadController.obtenerTodas(req, res, next));
+router.get('/:id', async (req, res, next) => await disponibilidadController.obtenerDisponibilidad(req, res, next));
+router.post('/', async (req, res, next) => await disponibilidadController.crearDisponibilidad(req, res, next));
+router.delete('/:id', async (req, res, next) => await disponibilidadController.eliminarDisponibilidad(req, res, next));
 // innecesario
 //router.put('/:id', (req, res) => disponibilidadController.actualizarDisponibilidad(req, res));
 
--- server/routes/turnoRoutes.js
+++ server/routes/turnoRoutes.js
@@ -3,11 +3,11 @@ import turnoController from "../controllers/turnoController.js";
 
 const router = Router();
 
-router.get('/', async (req, res) => await turnoController.obtenerTodos(req, res));
-router.get('/:id', async (req, res) => await turnoController.obtenerTurno(req, res));
-router.delete('/:id', async (req, res) => await turnoController.eliminarTurno(req, res));
-router.patch('/:id/darAlta', async (req, res) => await turnoController.darDeAlta(req, res));
-router.patch('/:id/darBaja', async (req, res) => await turnoController.darDeBaja(req, res));
-router.post('/crear', async (req, res) => await turnoController.crearTurno(req, res));
+router.get('/', async (req, res, next) => await turnoController.obtenerTodos(req, res, next));
+router.get('/:id', async (req, res, next) => await turnoController.obtenerTurno(req, res, next));
+router.delete('/:id', async (req, res, next) => await turnoController.eliminarTurno(req, res, next));
+router.patch('/:id/darAlta', async (req, res, next) => await turnoController.darDeAlta(req, res, next));
+router.patch('/:id/darBaja', async (req, res, next) => await turnoController.darDeBaja(req, res, next));
+router.post('/crear', async (req, res, next) => await turnoController.crearTurno(req, res, next));
 
 export default router;
