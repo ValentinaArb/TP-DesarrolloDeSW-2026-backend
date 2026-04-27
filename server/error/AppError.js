3cd89cab6a8e5c3fddfca59ab743ff51c4fc7a9a
BautiL <bleguia@frba.utn.edu.ar>
2026-04-26 23:32:16 -0300

    planteado AppError

---
+++ server/error/AppError.js
@@ -0,0 +1,29 @@
+class AppError extends Error{
+    constructor(mensaje,statusCode) {
+        super(mensaje);
+        this.name = this.constructor.name
+        this.statusCode = statusCode
+        this.status = statusCode >= 500 ? "error" : "fail"
+        this.timeStamp = new Date().toISOString();
+    }
+}
+
+export class BadRequestError extends AppError{
+    constructor(message) {super(message,400)}
+}
+
+export class NotFoundError extends AppError{
+    constructor(message) {super(message,404)}
+}
+
+export class ConflictError extends AppError{
+    constructor(message) {super(message,409)}
+}
+
+export class UnprocessableEntityError extends AppError{
+    constructor(message) {super(message,422)}
+}
+
+//HASTA MIN 49
+
+export { AppError};
