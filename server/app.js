0345af920b78eadf5202565a5defa7832c70546b
Valentina <70960916+ValentinaArb@users.noreply.github.com>
2026-05-06 23:46:18 -0300

    comienzo swagger

--- server/app.js
+++ server/app.js
@@ -5,13 +5,35 @@ import router from "./routes/router.js";
 import { errorLogger } from "./middlewaress/errorLogger.js";
 import { NotFoundHandler } from "./middlewaress/notFoundHandler.js";
 import { errorHandler } from "./middlewaress/errorHandler.js";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 
 const app = express();
 
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Documentación de API Sweet Medical',
+      version: '1.0.0',
+      description: 'Desarrollo de Software',
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT || 3000}`,
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
 app.use(express.json());
 app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(router);
 app.use(NotFoundHandler);
 app.use(errorLogger);
--- server/routes/router.js
+++ server/routes/router.js
@@ -3,14 +3,25 @@ import healthcheckRoutes from "./healthcheckRoutes.js";
 import turnoRoutes from "./turnoRoutes.js";
 import disponibilidadRoutes from "./disponibilidadRoutes.js";
 import medicoRoutes from "./medicoRoutes.js";
-import notificacionRoute from "./notificacionRoute.js";
+import notificacionesRoute from "./notificacionRoute.js";
 
 const router = express.Router()
-
+/**
+ * @openapi
+ * /usuarios:
+ * get:
+ * description: Retorna una lista de todos los usuarios
+ * responses:
+ * 200:
+ * description: Éxito
+ */
+router.get('/usuarios', (req, res) => {
+  res.send('Lista de usuarios');
+});
 router.use('/health',healthcheckRoutes);
 router.use('/turnos',turnoRoutes);
 router.use('/disponibilidades',disponibilidadRoutes);
 router.use('/medicos',medicoRoutes);
-router.use('/notificaciones',notificacionRoute);
+router.use('/notificaciones',notificacionesRoute);
 
 export default router
