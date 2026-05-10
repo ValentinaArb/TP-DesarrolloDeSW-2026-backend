d6ccc87216031d986caf15550eff187a677ef082
camiramosss <camiramos1810@gmail.com>
2026-05-10 11:36:16 -0300

    endpoint para busqueda de turnos con filtros

--- server/routes/turnoRoutes.js
+++ server/routes/turnoRoutes.js
@@ -3,10 +3,11 @@ import turnoController from "../controllers/turnoController.js";
 
 const router = Router();
 
+router.get('/busqueda', async (req, res, next) => await turnoController.buscarTurnosDisponibles(req, res, next));
 router.get('/', async (req, res,next) => await turnoController.obtenerTodos(req, res,next));
-router.get('/:id', async (req, res,next) => await turnoController.obtenerTurno(req, res,next));
-router.delete('/:id', async (req, res,next) => await turnoController.eliminarTurno(req, res,next));
-router.patch('/:id', async (req, res,next) => await turnoController.modificarEstado(req, res,next));
 router.post('/', async (req, res,next) => await turnoController.crearTurno(req, res,next));
+router.get('/:id', async (req, res,next) => await turnoController.obtenerTurno(req, res,next));
+router.patch('/:id', async (req, res,next) => await turnoController.modificarEstado(req, res,next));
+router.delete('/:id', async (req, res,next) => await turnoController.eliminarTurno(req, res,next));
 
 export default router;
