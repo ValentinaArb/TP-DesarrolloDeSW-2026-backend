a64948213ac790b34ba1753c3ae5654b4442c0db
Olaf Querol <oquerol@frba.utn.edu.ar>
2026-06-30 18:07:41 -0300

    Correccion en cron para que funcione

--- server/cron/recordatorioTurno.js
+++ server/cron/recordatorioTurno.js
@@ -1,19 +1,25 @@
-import { TurnoRepository } from "../repositories/turnoRepository"
-import { FactoryNotificacion } from "../domain/factoryNotificacion"
+import { TurnoRepository } from "../repositories/turnoRepository.js";
+import { FactoryNotificacion } from "../domain/factoryNotificacion.js";
+import cron from 'node-cron'; 
 
-const cron = require('node-cron');
-
-const turnoRepository = new TurnoRepository()
-const factoryNotificacion = new FactoryNotificacion()
+const turnoRepository = new TurnoRepository();
+const factoryNotificacion = new FactoryNotificacion();
 
 cron.schedule('0 8 * * *', async () => {
     console.log("Ejecutando cron de recordatorios...");
     
-    const maniana = new Date().setDate(maniana.getDate() + 1);
+    const hoy = new Date();
+    const maniana = new Date(hoy);
+    maniana.setDate(hoy.getDate() + 1);
 
-    const turnosParaRecordar = await turnoRepository.buscarPorFechaYEstado(maniana, 'RESERVADO');
+    try {
+        const turnosParaRecordar = await turnoRepository.buscarPorFechaYEstado(maniana, 'RESERVADO');
 
-    for (const turno of turnosParaRecordar) {
-        factoryNotificacion.crearRecordatorio(turno);
+        for (const turno of turnosParaRecordar) {
+            factoryNotificacion.crearRecordatorio(turno);
+        }
+        console.log(`Cron finalizado con éxito. Se procesaron ${turnosParaRecordar.length} turnos.`);
+    } catch (error) {
+        console.error("Error al ejecutar el cron de recordatorios:", error);
     }
 });
