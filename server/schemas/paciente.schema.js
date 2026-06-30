6cef47e6849b45039db71f9a1231dfef6d1a077e
Olaf Querol <oquerol@frba.utn.edu.ar>
2026-06-30 18:04:55 -0300

    Se agregaron las validaciones en schemas de medico y paciente para que no se puedan ingresar credenciales ni DNI con letras y fecha de nacimento sea mayor a 18 años

--- server/schemas/paciente.schema.js
+++ server/schemas/paciente.schema.js
@@ -18,11 +18,31 @@ const pacienteSchema = new mongoose.Schema({
     dni: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^\d+$/.test(v);
+            },
+            message: props => `El DNI [${props.value}] es inválido. Solo se permiten números.`
+        }
+
     },
     fechaNacimiento: {
         type: Date,
-        required: true
+        required: true,
+        validate: {
+            validator: function(valorFecha) {
+                if (!valorFecha) return false;
+                const hoy = new Date();
+                let edad = hoy.getFullYear() - valorFecha.getFullYear();
+                const diferenciaMeses = hoy.getMonth() - valorFecha.getMonth();
+                if (diferenciaMeses < 0 || (diferenciaMeses === 0 && hoy.getDate() < valorFecha.getDate())) {
+                    edad--;
+                }
+                return edad >= 18;
+            },
+            message: "El paciente debe ser mayor de 18 años para registrarse."
+        }
     },
     obraSocial: {
         nombre: { type: String }
