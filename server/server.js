2df8b23b9e53a6ad0eab454dff33af783eaac3e2
BautiL <bleguia@frba.utn.edu.ar>
2026-07-01 15:30:31 -0300

    cron turno

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 const port = process.env.PORT || 3001;
 await mongoose.connect(process.env.MONGODB_URI);
+recordatorioTurnoCron();
 app.listen(port, ()=>{
     console.info("El servidor arrancó correctamente en el puerto " + port);
 });
